8bc2b99bb1a7aa0da6ab0899b71acba475b649ae
Sam cotton <sam.cotton1234@gmail.com>
2019-03-13 09:55:19 +0000

    Trying to get the button to get the X and Y for the button pressed

--- src/Board.java
+++ src/Board.java
@@ -2,36 +2,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Board {
+public class Board implements ActionListener {
 
-		private JFrame frame = new JFrame();
-		private JPanel backBoard = new JPanel();
-		private int x = 0; 
-		private int y = 0; 
-		
-			
+	public static void main(String args[]) {
+		Board g = new Board();
+}
+	
+	private JFrame frame = new JFrame();
+	private JPanel backBoard = new JPanel();
+	private int x = 0; 
+	private int y = 0;
+	
+	int PieceColour = 1;
+	int PieceStatus = 0;
+	
 	public Board(){
-				
+		
+		int counter = 1;
+		int offset = 0; 
+		
 			frame.setTitle("Checkers");
 			backBoard.setLayout(null);
-			Square[] sqs = new Square[64];
-			int counter = 0;
-			int offset = 0; 
+			Square[] SquareBoard = new Square[65];
 			
 			for (int i = 0; i < 8; i++) {
 				for(int j = 0; j < 8; j++) {
 					
 					offset++;
-					sqs[counter] = new Square(backBoard, x, y, offset);
+					SquareBoard[counter] = new Square(backBoard, x, y, offset, PieceColour, counter);
+					SquareBoard[counter].returnButton().addActionListener(this);
 					counter++;
 					x += 100;
-					
 				}
-				
 				x = 0; 
 				y += 100; 
 				offset++;
-				
+				PieceColour++;
+
+								
 			}
 			frame.setContentPane(backBoard);
 			frame.setSize(900, 900);
@@ -40,4 +48,13 @@ public class Board {
 			
 				
 			}
+
+			public void actionPerformed(ActionEvent e){
+			int PosX  = Square.getX();
+		    System.out.println(PosX);
+		    
+		    int PosY = Square.getY();
+		    System.out.println(PosY);
+	
+		}	
 	}
--- src/Square.java
+++ src/Square.java
@@ -4,32 +4,91 @@ import java.awt.event.*;
 
 public class Square {
 	public static void main(String args[]) {
-
 		Board g = new Board();
-	
 	}
-  private JButton CheckerBoard = new JButton(); // Creates a new JButton Called CheckerBoard
+  JButton CheckerBoard = new JButton(); // Creates a new JButton Called CheckerBoard
+  int PiecePosition = 0;
   private int positionX; 						// Creates a variable for the X position called positionX
   private int positionY;						// Creates a variable for the Y position called positionY
   private int width = 100;						// Creates a variable for the width of the square called width
   private int height = 100;						// Creates a variable for the height of the square called height					
-  					
-    public Square( JPanel backBoard, int x, int y, int colour) {
-	  if (colour % 2 == 0) { 			// Determines if the variable colour is odd or even
-		
-		ImageIcon Black = new ImageIcon("C:/Users/Samuel Cotton/Documents/Uni/SCC110/Term2/Practicals/Assignment2/Git/pieces/empty2.png");
-	    CheckerBoard = new JButton(Black);
-			
+  int PieceStatus = 0;					// Creates a variable for the status of if a piece is on the button Square // this can be used in a if statement 
+  
+  
+    public Square( JPanel backBoard, int x, int y, int SquareColour, int Piece, int Counter) {
+    	PieceStatus = Piece;
+    	PiecePosition = Counter;    	
+    	
+	  if (SquareColour % 2 == 0) { 			// Determines if the variable colour is odd or even
+		  PieceStatus = 1;
+		 BlackBoard(); 
+		  
 	  }else {
-			
-			ImageIcon White = new ImageIcon("C:/Users/Samuel Cotton/Documents/Uni/SCC110/Term2/Practicals/Assignment2/Git/pieces/empty.png");
-		    CheckerBoard = new JButton(White);
-			
+		  	PieceStatus = 2;
+		  	WhiteBoard();
+		  
+			if (PieceStatus == 2 && PiecePosition >= 40) {
+				  PieceStatus = 3;
+				  WhitePiece();
+			  } 
+			if (PieceStatus == 2 && PiecePosition <= 25) {
+				PieceStatus = 4;
+				  RedPiece();	
+			}
 	  }
 	  CheckerBoard.setSize(width, height);
 	  positionX = x;
 	  positionY = y;
+  	  System.out.println(positionX);
+      System.out.println(positionY);
 	  CheckerBoard.setLocation(positionX, positionY);	
 	  backBoard.add(CheckerBoard);
+	  
+    }
+    
+	private void BlackBoard() {
+		
+		ImageIcon BlackBoard = new ImageIcon("C:/Users/Samuel Cotton/Documents/Uni/SCC110/Term2/Practicals/Assignment2/Git/pieces/empty2.png");
+		 CheckerBoard = new JButton(BlackBoard); 
 	}
-}
+	 
+	private void WhiteBoard() {
+		
+		ImageIcon White = new ImageIcon("C:/Users/Samuel Cotton/Documents/Uni/SCC110/Term2/Practicals/Assignment2/Git/pieces/empty.png");
+		CheckerBoard = new JButton(White);
+		
+
+	}
+	
+	public void WhitePiece() {
+	
+	Icon WhitePiece = new ImageIcon("C:/Users/Samuel Cotton/Documents/Uni/SCC110/Term2/Practicals/Assignment2/Git/pieces/white.png");
+	CheckerBoard = new JButton(WhitePiece);
+	
+	}
+	public void RedPiece() {
+		
+	Icon RedPiece = new ImageIcon("C:/Users/Samuel Cotton/Documents/Uni/SCC110/Term2/Practicals/Assignment2/Git/pieces/red.png");
+	CheckerBoard = new JButton(RedPiece);
+	
+	}
+	
+	
+	
+	public int getY() {
+		
+		return positionY;
+		
+	}
+	
+	public int getX() {
+		
+		return positionX;
+		
+		
+	}
+	public JButton returnButton() 
+	{
+	     return CheckerBoard;
+	}
+ }
